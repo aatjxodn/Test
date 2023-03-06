26927ae789a0f7705a6761ba774f137358635109
[tjxodn] <aatixodn@eksys.co.kr~>
2023-03-06 11:07:28 +0900

    20230306 특정 ip 접속 가능, alertutils 추가

--- src/main/java/egovframework/common/util/StringUtils.java
+++ src/main/java/egovframework/common/util/StringUtils.java
@@ -2,6 +2,8 @@ package egovframework.common.util;
 
 import java.security.MessageDigest;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class StringUtils {
 	// 비번 암호화 
 	public static String testSHA256(String txt) throws Exception{
@@ -21,4 +23,38 @@ public class StringUtils {
 	     
 	    return sbuf.toString();
 	}
+	
+	// ip 찾기 
+	public static String IPetRemoteAddr(HttpServletRequest request) {
+        String ip = null;
+        ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("Proxy-Client-IP"); 
+        } 
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("WL-Proxy-Client-IP"); 
+        } 
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("HTTP_CLIENT_IP"); 
+        } 
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR"); 
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("X-Real-IP"); 
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("X-RealIP"); 
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("REMOTE_ADDR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getRemoteAddr(); 
+        }
+        
+		return ip;
+	}
+
+	
 }
